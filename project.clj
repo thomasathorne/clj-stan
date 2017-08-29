acc65b89e47eae42d337eb24cc11f6efa29820e0
Thomas Athorne <thomas@athorne.net>
2017-08-29 18:42:30 +0100

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-stan "0.1.0-SNAPSHOT"
+(defproject clj-stan "0.1.1"
   :description "A library to interface with STAN, using the command line interface."
   :url "https://github.com/thomasathorne/clj-stan"
   :license {:name "Apache License, Version 2.0"
